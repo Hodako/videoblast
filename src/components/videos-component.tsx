082d1e3700be312fb76a4c99a058b3c4b15d02cd
dako shima <azizulhakim886@outlook.com>
2025-09-12 09:39:57 +0000

    The backend in the render.com is working fine and doing well. but when I

--- src/components/videos-component.tsx
+++ src/components/videos-component.tsx
@@ -1,4 +1,5 @@
 // src/components/videos-component.tsx
+'use client';
 import VideoCard from '@/components/video-card';
 import { getVideos } from '@/lib/data';
 import Link from 'next/link';
@@ -23,7 +24,7 @@ export default function VideosComponent({ currentPage }: { currentPage: number})
         try {
             const data = await getVideos({ sortBy: 'popular' });
             setAllVideos(data);
-        } catch (error) {
+        } catch (error) => {
             console.error("Failed to fetch videos:", error);
         } finally {
             setIsLoading(false);
